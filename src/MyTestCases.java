df735c2c5a9a6ff4d42da504c824672248bee28a
mais-jamil <maisjamil90@gmail.com>
2024-10-27 00:42:49 +0300

    add 4 tests

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,22 +9,19 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class MyTestCases {
-
 	WebDriver driver = new ChromeDriver();
 	String WebsiteURL = "https://global.almosafer.com/en";
+	Random rand = new Random();
 		
 	@BeforeTest
 	public void MySetUp() {
-		
 		driver.manage().window().maximize();
 		driver.get(WebsiteURL);
-		
 		WebElement CurrencyBtn = driver.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.cta__button.cta__saudi.btn.btn-primary"));
 		CurrencyBtn.click();
 	
 	}
-	
-	
+		
 	@Test(priority = 1)
 	public void CheckEnglishLangIsDefult() {
 		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
@@ -51,23 +51,42 @@ public class MyTestCases {
 		boolean ActualResult = driver.findElement(By.cssSelector(".sc-ghsgMZ.hIElfs")).isDisplayed();
 		boolean ExpectedResult = true;
 		org.testng.Assert.assertEquals(ActualResult, ExpectedResult);
+		
 	}
 	
 	@Test(priority = 5)
 	public void CheckTheHotelNotSelected() {
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		String ActualValue = HotelTab.getAttribute("aria-selected");
+		String ExpextedValue = "false";
+		org.testng.Assert.assertEquals(ActualValue, ExpextedValue);
 		
 	}
 	
 	@Test(priority = 6)
 	public void CheckDepatureDate() {
-		
+		int Tommorw = LocalDate.now().plusDays(1).getDayOfMonth();
+		String ActualDepature = driver.findElement(By.cssSelector("div[class='sc-OxbzP sc-lnrBVv gKbptE'] span[class='sc-fvLVrH hNjEjT']")).getText();
+		String ExpextedDepature = Integer.toString(Tommorw);
+		org.testng.Assert.assertEquals(ActualDepature, ExpextedDepature);
+					
 	}
 	
 	@Test(priority = 7)
 	public void CheckReturnDate() {
+		int AfterTommorw = LocalDate.now().plusDays(2).getDayOfMonth();
+		String ActualReturn = driver.findElement(By.cssSelector("div[class='sc-OxbzP sc-bYnzgO bojUIa'] span[class='sc-fvLVrH hNjEjT']")).getText();
+		String ExpextedReturn = Integer.toString(AfterTommorw);
+		org.testng.Assert.assertEquals(ActualReturn, ExpextedReturn);
 		
 	}
 	
-	
+	@Test(priority = 8, invocationCount = 5)
+	public void RandomlyChangeTheLanguage() {
+		String [] MyWebsites = {"https://www.almosafer.com/en", "https://www.almosafer.com/ar"};
+		int randomIndex = rand.nextInt(MyWebsites.length);
+		driver.get(MyWebsites[randomIndex]);
+		
+	}
 	
 }
